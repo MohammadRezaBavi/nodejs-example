cefc89ca3dab146d3026b341fafab987e373b8e5
Mohammad Reza <Mohammad1380.new@gmail.com>
2018-12-20 17:23:41 +0330

    Initial

---
+++ index.js
@@ -0,0 +1,28 @@
+const express = require("express");
+var app = express();
+const hbs = require("hbs");
+
+app.set('view engine','hbs');
+hbs.registerPartials(__dirname + '/views/partials');
+app.use(express.static(__dirname + "/public"));
+
+app.use((req,res,next) => {
+
+var url = req.url;
+  res.render("maintenance.hbs");
+
+next();
+
+});
+
+hbs.registerHelper('getTime',() => {
+  return "Mohammad Reza2";
+});
+
+app.get("/",(req,res) => {
+  res.render('index.hbs',{
+  });
+});
+app.listen(3000,() => {
+  console.log("Server is Running on port 3000");
+});
